5bb241edbf9f49b776a0ea474bfbc23859e65514
Warren Fletcher <warren@fletch.band>
2022-05-10 12:08:20 +0200

    First Commit

---
+++ network.py
@@ -0,0 +1,16 @@
+class Network():
+    def __init__(self) -> None:
+        pass
+    def tick(self) -> None:
+        pass
+class Intersection():
+    def __init__(self) -> None:
+        pass
+
+class Road():
+    def __init__(self) -> None:
+        pass
+
+class Car():
+    def __init__(self) -> None:
+        pass
