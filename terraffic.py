7a6dbc87942ed172d0791c01b5b4eb9e6ce94c6e
Warren Fletcher <warren@fletch.band>
2022-05-26 16:08:01 +0200

    Added Automatic and Manual Mode

--- terraffic.py
+++ terraffic.py
@@ -3,7 +3,18 @@ import sys
 if __name__ == "__main__":
     print("Teraffic Network Simulation")
     name = sys.argv[1]
+    mode = sys.argv[2]
+    enabled_modes = ["manual", "auto"]
+    mode = mode.lower()
+    if mode not in enabled_modes:
+        raise Exception("Invalid Mode. Valid Modes are {}".format(enabled_modes))
     if name == None:
         raise Exception("Please Provide the name of a config!")
+    print("Building: {} with Mode: {}".format(name, mode))
     network = Network(name)
+    print(network)
+    if mode == "auto":
+        ticks = int(sys.argv[3])
+        for _ in range(ticks):
+            network.tick()
     
